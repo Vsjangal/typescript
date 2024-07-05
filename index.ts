310c6def582cab309faaa70f5489c0efa13d2731
Vijay Jangal <jangalvijay36@gmail.com>
2024-07-05 21:56:25 +0530

    initial commit

--- index.ts
+++ index.ts
@@ -131,3 +131,15 @@ numArray.push(1);
 stringArray.push("name");
 
 
+class VirtualPoint {
+  x: number;
+  y: number;
+ 
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+}
+ 
+const newVPoint = new VirtualPoint(13, 56);
+console.log(newVPoint.x, newVPoint.y); 
